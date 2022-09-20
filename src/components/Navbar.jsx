eddd50d5cc5dcc53b71172f7442559afbac5f07f
Victor Tran <101998990+vic-tran@users.noreply.github.com>
2022-09-20 12:50:56 -0700

    Navbar routes and styling
    added routing to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,18 +7,34 @@ const Navbar = () => {
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100] text-current'>
             <div className='flex justify-between items-center w-full px-2 2xl:px-16'> 
-            <a href="/" className='cursor-pointer'>
-                <img src={GT} alt='/' width='125' height='75'/>    
-            </a>   
-            <div>
-                <a className='ml-10 text-lg uppercase hover:border-b cursor-pointer' href="/">Home</a>
-                <a className='ml-10 text-lg uppercase hover:border-b' href="/about"> About</a>
-                <a className='ml-10 text-lg uppercase hover:border-b' href="/events"> Events</a>
-                <a className='ml-10 text-lg uppercase hover:border-b' href="/architectural"> Architectural</a>
-                <a className='ml-10 text-lg uppercase hover:border-b' href="/amenities"> Amenities</a>
-                <a className='ml-10 text-lg uppercase hover:border-b' href="/support"> Support</a>
-                <a className='ml-10 text-lg uppercase hover:border-b' href="/contact"> Contact</a>
-            </div>
+                <a href="/" className='cursor-pointer'>
+                    <img src={GT} alt='/' width='125' height='75'/>    
+                </a>   
+                <div>
+                    <ul className='flex justify-between items-center w-full px-5 2xl:px-16 ml-5 text-lg uppercase cursor-pointer'>
+                        <li>  
+                            <Link to="/" className='ml-10 text-lg uppercase hover:border-b'>Home</Link> 
+                        </li>
+                        <li>  
+                            <Link to="/about" className='ml-10 text-lg uppercase hover:border-b'>About</Link>
+                        </li>
+                        <li>  
+                            <Link to="/events" className='ml-10 text-lg uppercase hover:border-b'>Events</Link>
+                        </li>
+                        <li>  
+                            <Link to="/architectural" className='ml-10 text-lg uppercase hover:border-b'> Architectural</Link>
+                        </li>
+                        <li>
+                            <Link to="/amenities" className='ml-10 text-lg uppercase hover:border-b'> Amenities</Link>
+                        </li>
+                        <li> 
+                            <Link to="/support" className='ml-10 text-lg uppercase hover:border-b'>Support</Link>
+                        </li>
+                        <li>  
+                            <Link to="/contact" className='ml-10 text-lg uppercase hover:border-b'>Contact</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App/>} />
+    </Routes>
+  </Router>,
+
+  document.getElementById('root')
+
+
+
+
+
 );
 
