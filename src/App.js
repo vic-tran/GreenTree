205a882bdfa9ac05aa188a0cec539c95dcbc4c98
Shiva Chemburkar <samirshivachemburkar@gmail.com>
2022-09-20 16:37:27 -0700

    Architectural Page Updates
    Updates to Architectural file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Main from './components/Main';
-import Navbar from './components/Navbar';
-
-
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import Architectural from "./pages/Architectural";
+import Amenities from "./pages/Amenities";
+import Support from "./pages/Support";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Main />
+      <About />
+      <Events />
+      <Architectural />
+      <Amenities />
+      <Support />
+      <Contact />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import videoBg from '../assets/bg.mp4'
+import React from "react";
+import videoBg from "../assets/bg.mp4";
 
 export const main = () => {
   return (
-    <div className='main'>
-    <div className='overlay'></div>
-        <video src={videoBg} autoPlay loop muted />
-        <div className='content'>
-            <h1>Welcome to Greentree Homes</h1>
-        </div>
+    <div className="main">
+      <div className="overlay"></div>
+      <video src={videoBg} autoPlay loop muted />
+      <div className="content">
+        <h1>Welcome to Greentree Homes</h1>
+      </div>
+      <div className="content">
+        <h2>
+          Irvine's hidden gem with 626 single family homes at the city's center.
+        </h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default main;
