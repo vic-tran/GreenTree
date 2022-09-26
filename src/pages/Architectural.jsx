4e15fb80bccbef320576f7f36116eb76f53013a6
Shiva Chemburkar <samirshivachemburkar@gmail.com>
2022-09-26 12:00:32 -0700

    Architectural, About, and Footer

--- src/pages/Architectural.jsx
+++ src/pages/Architectural.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import plan1 from "../assets/Plan10x.png";
 import plan2 from "../assets/Plan21x.png";
@@ -39,3 +40,46 @@ const Architectural = () => {
 };
 
 export default Architectural;
+=======
+import React from "react";
+import plan1 from "../assets/Plan10x.png";
+import plan2 from "../assets/Plan21x.png";
+import plan3 from "../assets/Plan31x.png";
+import plan4 from "../assets/Plan40x.png";
+import plan5 from "../assets/Plan51x.png";
+import plan6 from "../assets/Plan70x.png";
+import ArchitecturalInfo from "./ArchitecturalInfo";
+
+const Architectural = () => {
+  return (
+    <div className="contentBackground">
+      <div className="Architectural">
+        <h1>Home Types</h1>
+      </div>
+      <div class="grid gap-x-1 gap-y-10 grid-cols-3 ">
+        <div className="transform h-64 bg-blue-400 w-80 transition duration-500 hover:scale-125 hover:bg-blue-600 flex justify-center items-center">
+          <img class=".object-cover h-78 w-96 ..." src={plan1} alt="" />
+        </div>
+        <div className="transform h-64 bg-blue-400 w-80 transition duration-500 hover:scale-125 hover:bg-blue-600 flex justify-center items-center">
+          <img class=".object-cover h-78 w-96 ..." src={plan2} alt="" />
+        </div>
+        <div className="transform h-64 bg-blue-400 w-80 transition duration-500 hover:scale-125 hover:bg-blue-600 flex justify-center items-center">
+          <img class=".object-cover h-78 w-96 ..." src={plan3} alt="" />
+        </div>
+        <div className="transform h-64 bg-blue-400 w-80 transition duration-500 hover:scale-125 hover:bg-blue-600 flex justify-center items-center">
+          <img class=".object-cover h-78 w-96 ..." src={plan4} alt="" />
+        </div>
+        <div className="transform h-64 bg-blue-400 w-80 transition duration-500 hover:scale-125 hover:bg-blue-600 flex justify-center items-center">
+          <img class=".object-cover h-78 w-96 ..." src={plan5} alt="" />
+        </div>
+        <div className="transform h-64 bg-blue-400 w-80 transition duration-500 hover:scale-125 hover:bg-blue-600 flex justify-center items-center">
+          <img class=".object-cover h-78 w-96 ..." src={plan6} alt="" />
+        </div>
+      </div>
+      <ArchitecturalInfo />
+    </div>
+  );
+};
+
+export default Architectural;
+>>>>>>> Stashed changes
