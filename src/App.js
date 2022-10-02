a7d19c375b8098681d6e489a22bed0d4a6746416
Austin J. Alexander <austinjalexander@gmail.com>
2022-10-02 10:20:27 -0700

    feat(pages): add plans, pages, etc.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Main from './components/Main';
-import Navbar from './components/Navbar';
-
-
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Main />
-    </div>
+    <html class="scroll-smooth">
+      <div>
+        <Navbar />
+        <Main />
+        <Footer />
+      </div>
+    </html>
   );
 }
 
